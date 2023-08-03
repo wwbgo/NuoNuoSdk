ee0604e92dc3a96b1e5d7d6c5b2f0c30f39c6774
wwb <wwbgo@qq.com>
2023-08-03 18:21:34 +0800

    add some extension

---
+++ src/NuoNuoSdk/NuoNuoSdkExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace NuoNuoSdk
+{
+    public static class NuoNuoSdkExtension
+    {
+        public static string ToInvoiceDatString(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
